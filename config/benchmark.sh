f4a190790a493209481d1f9ce2fbeac0d8be3f1f
Shogo OKADA <shogo.okada@kek.jp>
2025-07-08 13:01:18 +0900

    modify configurations

--- config/benchmark.sh
+++ config/benchmark.sh
@@ -2,7 +2,7 @@
 #===============================================================================
 #  BSD 2-Clause License
 #
-#  Copyright (c) 2021-2024 Shogo OKADA (shogo.okada@kek.jp)
+#  Copyright (c) 2021-2025 Shogo OKADA (shogo.okada@kek.jp)
 #  All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
@@ -46,26 +46,36 @@ config_file="config_bench.json"
 make_config_file() {
 cat << EOF > ${config_file}
 {
-  "random_seed"           : 123456789,
-  "event_number"          : $1,
-  "thread_number"         : $2,
-  "cpu_affinity"          : false,
-  "beam_particle"         : "e-",
-  "beam_ion_Z"            : 0,
-  "beam_ion_A"            : 0,
-  "beam_energy"           : 750,
-  "beam_source_pos"       : [0.0, 0.0, 0.0],
-  "beam_direction"        : [0.0, 0.0, 1.0],
-  "target_size"           : [20.0, 20.0, 20.0],
-  "primary_removal"       : true,
-  "kill_energy"           : [75.0, 75.1],
-  "phys_list"             : "G4EmDNAPhysics_option8",
-  "chem_list"             : "G4EmDNAChemistry_option3",
-  "time_step_model"       : "IRT",
-  "ele_solvation_model"   : "Meesungnoen2002",
-  "use_molecule_counter"  : false,
+  "random_seed"     : 123456789,
+  "event_number"    : $1,
+  "thread_number"   : $2,
+  "cpu_affinity"    : false,
+  "beam_particle"   : "e-",
+  "beam_ion_Z"      : 0,
+  "beam_ion_A"      : 0,
+  "beam_energy"     : 750.0,
+  "beam_source_pos" : [0.0, 0.0, 0.0],
+  "beam_direction"  : [0.0, 0.0, 1.0],
+  "target_size"     : [20.0, 20.0, 20.0],
+  "physics_configs" : {
+    "physics_option"           : "G4EmDNAPhysics_option8",
+    "electron_solvation_model" : "Meesungnoen2002",
+    "use_multiple_ionisation"  : false,
+    "primary_removal_configs"  : {
+      "enabled"     : true,
+      "kill_energy" : [75.0, 75.1]
+    }
+  },
+  "chemistry_configs": {
+    "chemistry_option" : "G4EmDNAChemistry_option3",
+    "time_step_model"  : "IRT",
+    "use_alternative_B1A1_decay"   : false,
+    "use_alternative_decay_vibH2O" : false,
+    "use_g4_molecule_counter"      : true,
+    "simulation_end_time"          : 1.0E+06
+  },
   "check_boundary"        : false,
-  "output_file"           : "$3",
+  "output_gval"           : "$3",
   "benchmark_file"        : "$4",
   "benchmark_for_threads" : false,
   "term_frequency"        : $5
@@ -151,12 +161,12 @@ for it in ${threads[@]}; do
     event_num=$((it * base_event))
   fi
 
-  output_file="result_${it}mt.csv"
+  output_file="result_gval_${it}mt.csv"
   benchmark_file="benchmark_${it}mt.json"
   term_freq=$((event_num / 10))
 
   # make a configuration file
-  make_config_file $event_num $it $output_file $benchmark_file $term_freq
+  make_config_file ${event_num} ${it} ${output_file} ${benchmark_file} ${term_freq}
 
   # set a simulation log file
   log_file="run_${it}mt.log"
